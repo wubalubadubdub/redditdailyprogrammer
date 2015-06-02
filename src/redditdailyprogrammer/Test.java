bed710ee2b7c35a2c37d5f75268a6d719ae02d60
Intensive Porpoises <wubalubadubdub@users.noreply.github.com>
2015-06-02 12:12:39 -0400

    split into two files

---
+++ src/redditdailyprogrammer/Test.java
@@ -0,0 +1,18 @@
+package redditdailyprogrammer;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList <String> dealt = new ArrayList<String>();
+		dealt.add("5h");
+		dealt.add("2c");
+		
+		System.out.println(dealt.contains("5h"));
+
+	}
+
+}
--- src/redditdailyprogrammer/TexasHoldThem.java
+++ src/redditdailyprogrammer/TexasHoldThem.java
@@ -15,8 +15,27 @@ public class TexasHoldThem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		getPlayerCount();
-		Player You = new Player();
+		HoldemPlayer You = new HoldemPlayer();
+		HoldemPlayer p2 = new HoldemPlayer();
+		HoldemPlayer p3 = new HoldemPlayer();
+		HoldemPlayer p4 = new HoldemPlayer();
+		HoldemPlayer p5 = new HoldemPlayer();
+		HoldemPlayer p6 = new HoldemPlayer();
+		HoldemPlayer p7 = new HoldemPlayer();
+		HoldemPlayer p8 = new HoldemPlayer();
+		
+		
+		
+		
 		You.dealHand();
+		p2.dealHand();
+		p3.dealHand();
+		p4.dealHand();
+		p5.dealHand();
+		p6.dealHand();
+		p7.dealHand();
+		p8.dealHand();
+		
 
 	}
 
@@ -29,76 +48,6 @@ public class TexasHoldThem {
 
 }
 
-class Player {
 
-	ArrayList<String> ranks = new ArrayList<String>(Arrays.asList("2", "3",
-			"4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"));
 
-	ArrayList<String> suits = new ArrayList<String>(Arrays.asList("s", "d",
-			"c", "h"));
 
-	ArrayList<String> dealt = new ArrayList<String>(); // keeps track of hands
-														// already dealt
-
-	// assign vars for the rank of each random card
-	Random randRank = new Random();
-
-	// and for the suit of each random card
-	Random randSuit = new Random();
-
-	public void dealHand() {
-		// before dealing a card, check to see if it's already been dealt
-		int rankChoice;
-		int suitChoice;
-
-		// strings for ranks and suits
-		String r1;
-		String r2;
-		String s1;
-		String s2;
-
-		// strings for cards
-		String c1;
-		String c2;
-		
-		String hand;
-
-		do {
-
-			// generate first random card
-
-			rankChoice = randRank.nextInt(ranks.size());
-			suitChoice = randSuit.nextInt(suits.size());
-
-			r1 = ranks.get(rankChoice);
-			s1 = suits.get(suitChoice);
-			c1 = r1 + s1;
-
-		} while (dealt.contains(c1));
-		
-		dealt.add(c1); // when a card is dealt, add it to the arraylist of dealt
-						// cards so we won't deal it again
-		
-		do {
-			
-		
-
-		// generate second random card
-
-		rankChoice = randRank.nextInt(ranks.size());
-		suitChoice = randSuit.nextInt(suits.size());
-		
-		r2 = ranks.get(rankChoice);
-		s2 = suits.get(suitChoice);
-		c2 = r2 + s2;
-		
-		} while (dealt.contains(c2));
-		
-		dealt.add(c2);
-		hand = c1 + " " + c2; //hand is the concatenation of the two cards w/space in between
-		System.out.println("You: " + hand);
-		
-
-	}
-
-}
